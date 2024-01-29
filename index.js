8f3481a1a362dd3f10f61e43f4d5d6856373386d
ashley <revesfideles@gmail.com>
2024-01-29 18:23:44 -0500

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+ function destructivelyPrependCat(name) {
+    cats.unshift(name);
+ }
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(Broom) {
+    const newCats = [...cats, Broom];
+    return newCats;
+}
+function prependCat(Arnold) {
+    const newCats = [Arnold, ...cats];
+    return newCats;
+}
+function destructivelyRemoveLastCat() {
+    const newCats = cats.pop();
+    return newCats;
+}
+function removeFirstCat() {
+   const newCats = cats.slice(1);
+    return newCats;
+}
+function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+}
